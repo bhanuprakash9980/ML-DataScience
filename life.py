aac988339c0e08a2186e073a6985c5af4984ddd6
bhanuprakash9980 <bhanuprakashshettigar@gmail.com>
2021-04-22 18:07:40 +0530

    first commit

---
+++ life.py
@@ -0,0 +1,10 @@
+from math import sqrt
+theAnswer = 42
+
+
+def quote_marvin():
+    print(' I\'ve calculated your chance of survival, but I don\'t think you\'ll like it. ')
+
+
+def square_root(a):
+    return(sqrt(a))
